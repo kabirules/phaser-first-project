7bdafc54f04e4c43952cc32a40bc2a77df651272
kabi_rules <javifont@gmail.com>
2021-02-27 18:49:36 +0100

    firebase test - google login

---
+++ firebase-test/src/scenes/HelloWorldScene.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import FirebaseConfig from '../config/firebaseConfig'
+
+export default class HelloWorldScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('hello-world')
+	}
+
+	preload() {
+
+    }
+
+    create() {
+        console.log(FirebaseConfig.firebaseConfig)
+        var firebaseApp = firebase.initializeApp(FirebaseConfig.firebaseConfig);
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        const googleButton = this.add.text(350, 300, 'Google login', { fill: '#0f0' });
+        googleButton.setInteractive();
+        googleButton.on('pointerdown', () => { 
+            this.googleLogin(firebaseApp, provider); 
+        });
+    }
+
+    googleLogin(firebaseApp, provider) {
+        firebaseApp.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+            console.log(user);
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+        });
+    }
+}
