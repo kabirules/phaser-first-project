68db71404c3392482951b9eff091507ac73d03ee
kabi_rules <javifont@gmail.com>
2021-03-03 23:23:10 +0100

    planes - playing around with engine

--- planes/scenes/game.js
+++ planes/scenes/game.js
@@ -14,13 +14,14 @@ export class Game extends Phaser.Scene {
         this.plane.setCollideWorldBounds(true);
         //this.plane.setDamping(true);
         this.plane.setDrag(0.99);
-        this.plane.setMaxVelocity(200);
+        this.plane.setMaxVelocity(150);
         // INPUT
         this.cursors = this.input.keyboard.createCursorKeys();
         this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
+        console.log(this.plane.body.velocity)
         if (this.cursors.left.isDown) {
             this.plane.body.angularVelocity = -100
         } else if (this.cursors.right.isDown) {
@@ -29,11 +30,16 @@ export class Game extends Phaser.Scene {
             this.plane.body.angularVelocity = 0
         }
         if (this.keySpace.isDown) {
-            this.physics.velocityFromRotation(this.plane.rotation, 50, this.plane.body.acceleration);
+            this.physics.velocityFromRotation(this.plane.rotation, 80, this.plane.body.acceleration);
         } else {
-            this.plane.setAcceleration(0)
-            this.plane.body.velocity.x = Math.abs(this.plane.body.velocity.x - 10)
-            this.plane.body.velocity.y = Math.abs(this.plane.body.velocity.y - 10)
+            this.plane.body.velocity.x = this.plane.body.velocity.x -1
+            if (this.plane.body.velocity.x < 0) {
+                this.plane.body.velocity.x = 0
+            }
+            this.plane.body.velocity.y = this.plane.body.velocity.y +1
+            if (this.plane.body.velocity.y < 0) {
+                this.plane.body.velocity.y = 0
+            }
         }
         this.physics.world.wrap(this.plane, 32)
     }
